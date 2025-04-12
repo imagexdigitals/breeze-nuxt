4651b3625ac9969c43f3e6ba52501ae1abebec5c
Gopinaath K <imagexdigitals@gmail.com>
2025-04-12 18:36:12 +0530

    feat: Refactor header components for improved mobile detection and responsiveness

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,24 +1,32 @@
 <script lang="ts" setup>
-import { useMobileDetection } from '~/composables/useMobileDetection'
 import { onMounted } from 'vue'
-
 const { isAuthenticated } = useSanctumAuth()
-
-// Use the composable to get the isMobile state
-const { isMobile } = useMobileDetection()
-
-// Log the initial state
-onMounted(() => {
-  console.log('Initial isMobile state:', isMobile.value)
-})
 </script>
 
 <template>
   <div>
-    <HeaderDesktop v-if="!isMobile" />
-    <HeaderMobile v-else />
+    <HeaderDesktop class="header-desktop" />
+    <HeaderMobile class="header-mobile" />
     <slot />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header-desktop {
+  display: none;
+}
+
+.header-mobile {
+  display: block;
+}
+
+@media (min-width: 768px) {
+  .header-desktop {
+    display: block;
+  }
+
+  .header-mobile {
+    display: none;
+  }
+}
+</style>
